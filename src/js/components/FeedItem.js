6b3514268ed1fe032630e2952cc21a0d53b56f6e
Joe Hull <josephghull@gmail.com>
2018-05-07 18:48:28 -0400

    Add components to project

---
+++ src/js/components/FeedItem.js
@@ -0,0 +1,12 @@
+var React = require('React');
+
+var FeedItem = React.createClass({
+
+    render: function() {
+        return (
+            <div />
+        );
+    }
+});
+
+module.exports = FeedItem;
